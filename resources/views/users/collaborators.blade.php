20f31615649a1afa987b68c122dd713278de4239
ninitas1992 <120388115+ninitas1992@users.noreply.github.com>
2023-05-15 15:53:16 +0200

    .

---
+++ resources/views/users/collaborators.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Collaborator</title>
+
+    <!-- Font Awesome -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.css"
+    rel="stylesheet"/>
+
+
+</head>
+<body>
+    <div class="container">
+
+        <table class="table align-middle mb-0 bg-white">
+            <thead class="bg-light">
+              <tr>
+                <th>Collaborator</th>        
+                <th>Perfil</th>        
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+
+                @foreach ($users as $user)
+
+              <tr>
+                <td>
+                  <div class="d-flex align-items-center">
+                    <img
+                        src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                        alt=""
+                        style="width: 45px; height: 45px"
+                        class="rounded-circle"
+                        />
+                    <div class="ms-3">
+                      <p class="fw-bold mb-1">{{$user->name}}</p>
+                      <p class="text-muted mb-0">john.doe@gmail.com</p>
+                    </div>
+                  </div>
+                </td>        
+                <td>
+                  <span class="badge badge-success rounded-pill d-inline">Active</span>
+                </td>
+                
+                <td>
+                  <button type="button" class="btn btn-link btn-sm btn-rounded">
+                    Edit
+                  </button>
+                </td>    
+             
+              </tr>
+                @endforeach
+
+            </tbody>
+          </table>
+
+    </div>
+
+    
+
+    
+</body>
+</html>
+
+
