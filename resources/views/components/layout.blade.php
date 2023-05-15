d44b9b9c0f030960b976e1db651dec452a32d345
Paulo Granadas <paulogranadas@gmail.com>
2023-05-15 18:37:58 +0200

    pd

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,6 +17,18 @@
 <body>
 
     <header>
+    <style>
+        /* Height for devices larger than 576px */
+        @media (min-width: 992px) {
+          #intro {
+            margin-top: -58.59px;
+          }
+        }
+
+        .navbar .nav-link {
+          color: #fff !important;
+        }
+      </style>
 
         <!-- Navbar -->
         <nav class="navbar navbar-expand-lg navbar-dark d-none d-lg-block" style="z-index: 2000;">
@@ -45,26 +57,59 @@
                         </li>
                     </ul>
 
-                    <ul class="navbar-nav d-flex flex-row">
-                        <!-- Icons -->
-                        <li class="nav-item me-3 me-lg-0">
-                            <a class="nav-link" href="/" rel="nofollow" target="_blank">
-                                <i class="fab fa-youtube"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item me-3 me-lg-0">
-                            <a class="nav-link" href="/" rel="nofollow" target="_blank">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                        </li>
+                <ul class="navbar-nav d-flex flex-row">
+                    <!-- Icons -->
+                    <li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" rel="nofollow" target="_blank">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://www.facebook.com/mdbootstrap" rel="nofollow" target="_blank">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://twitter.com/MDBootstrap" rel="nofollow" target="_blank">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://github.com/mdbootstrap/mdb-ui-kit" rel="nofollow" target="_blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar -->
 
-                    </ul>
+    <!-- Background image -->
+    <div id="intro" class="bg-image vh-100 shadow-1-strong">
+        <video style="min-width: 100%; min-height: 100%;" playsinline autoplay muted loop>
+            <source class="h-100" src="videos/backvideo.mp4" type="video/mp4" />
+        </video>
+        <div class="mask" style="
+            background: linear-gradient(
+              45deg,
+              rgba(29, 236, 197, 0.7),
+              rgba(91, 14, 214, 0.7) 100%
+            );
+          ">
+            <div class="container d-flex align-items-center justify-content-center text-center h-100">
+                <div class="text-white">
+                    <h1 class="mb-3">Spherart</h1>
+                    <h5 class="mb-4">Collaborate True Creativity</h5>
+                    <a class="btn btn-outline-light btn-lg m-2" href="https://www.youtube.com/watch?v=c9B4TPnak1A" role="button" rel="nofollow" target="_blank">Projects</a>
+                    <a class="btn btn-outline-light btn-lg m-2" href="https://mdbootstrap.com/docs/standard/" target="_blank" role="button">Collaborators</a>
                 </div>
             </div>
-        </nav>
-        <!-- Navbar -->
-        @include('partials._introBackground')
-    </header>
+        </div>
+    </div>
+    <!-- Background image -->
+</header>
+<!--Main Navigation-->
 
 
     <!-- MDB -->
