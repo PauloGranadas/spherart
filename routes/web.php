eb90789201d4a5ac5c28ca7fc5773dd0ab98c5a2
Paulo Granadas <110468245+PauloGranadas@users.noreply.github.com>
2023-05-16 10:51:32 +0200

    ui

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/collaborators', [UserController::class, 'showCollaborators']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
