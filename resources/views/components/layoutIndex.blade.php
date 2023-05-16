ec0261a525a5ee040fb729650edf2e4918125a08
Paulo Granadas <110468245+PauloGranadas@users.noreply.github.com>
2023-05-16 10:26:33 +0200

    Update layoutIndex.blade.php

--- resources/views/components/layoutIndex.blade.php
+++ resources/views/components/layoutIndex.blade.php
@@ -109,32 +109,9 @@
             <!-- Container wrapper -->
         </nav>
         <!-- Navbar -->
-        <!-- Navbar -->
 
         <!-- Background image -->
-        <!-- <div id="intro" class="bg-image vh-100 shadow-1-strong">
-            <video style="min-width: 100%; min-height: 100%;" playsinline autoplay muted loop>
-                <source class="h-100" src="videos/backvideo.mp4" type="video/mp4" />
-            </video>
-            <div class="mask" style="
-            background: linear-gradient(
-              45deg,
-              rgba(29, 236, 197, 0.7),
-              rgba(91, 14, 214, 0.7) 100%
-            );
-          ">
-                <div class="container d-flex align-items-center justify-content-center text-center h-100">
-                    <div class="text-white">
-                        <img src="images/spherart_w.png" alt="" width="75%">
-                        <h2>Connect True Creativity</h2>
 
-                        <a class="btn btn-outline-light btn-lg m-2" href="https://www.youtube.com/watch?v=c9B4TPnak1A"
-                            role="button" rel="nofollow" target="_blank">Projects</a>
-                        <a class="btn btn-outline-light btn-lg m-2" href="collaborators" role="button">Collaborators</a>
-                    </div>
-                </div>
-            </div>
-        </div> -->
         <!-- Background image -->
     </header>
     <!--Main Navigation-->
