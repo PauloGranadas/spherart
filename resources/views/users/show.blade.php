e5677890db1ff9b014dfea55bbbc34c86b6fb30e
ninitas1992 <120388115+ninitas1992@users.noreply.github.com>
2023-05-16 15:59:37 +0200

    .

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <x-layout>
     <div class="grid">
         <div class="g-col-4">
@@ -16,3 +17,27 @@
         </div>
       </div>
 </x-layout>
+=======
+<x-layoutIndex>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-3">
+                <img
+                    src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp"
+                    class="img-fluid hover-shadow"
+                    alt="Los Angeles Skyscrapers"
+                    />
+            </div>
+            <div class="col-8">
+                <p class="fs-4">Joe Doe</p>
+                <p class="fs-4">email@gmail.com</p>
+                <p class="lh-1">
+                    Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio
+                    dui.
+                </p>
+            </div>
+          </div>
+    </div>
+    
+</x-layoutIndex>
+>>>>>>> Stashed changes
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/collaborators', [UserController::class, 'showCollaborators']);
   
 // page detail collaborators
 Route::get('/collaborators/{user}', [UserController::class, 'show']);
+
+// page detail collaborators
+Route::get('/register', [UserController::class, 'show']);
