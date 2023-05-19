4fcdae2604633148ef8c7f628623988afb1d1bd9
bebel1982 <120385999+bebel1982@users.noreply.github.com>
2023-05-19 11:12:06 +0200

    Update show.blade.php

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -36,6 +36,24 @@
 
 </main>
 
+
+<div class="card">
+  <img
+    src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"
+    class="card-img-top"
+    alt="hollywood sign"
+  />
+  <div class="card-body">
+    <h5 class="card-title">Card with stretched link</h5>
+    <p class="card-text">
+      Some quick example text to build on the card title and make up the bulk of the
+      card's content.
+    </p>
+    <a href="#!" class="btn btn-primary stretched-link">Go somewhere</a>
+  </div>
+</div>
+
+
 </body>
 
 <footer class="bg-light text-center text-white">
