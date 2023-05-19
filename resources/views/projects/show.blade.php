7780df5ff4e4a16b620ac616005e8b527319440d
bebel1982 <120385999+bebel1982@users.noreply.github.com>
2023-05-19 10:46:08 +0200

    update d page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,4 +2,15 @@
 
     <h1>Show detail</h1>
 
+    <nav>
+      <center>
+        <div class="d-inline-flex position-relative">
+        <span class="position-absolute top-0 start-100 translate-middle p-1 bg-danger border border-light rounded-circle">
+          <span class="visually-hidden">New alerts</span>
+        </span>
+        <img class="rounded-4 shadow-4" src="https://mdbootstrap.com/img/Photos/Avatars/man2.jpg" alt="Avatar" style="width: 50px; height: 50px;">
+        </div>
+        </center>
+    </nav>
+
 </x-layoutIndex>
