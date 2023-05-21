1a2885171f6d835fd7606f5d897d7a0951d0fba6
bebel1982 <120385999+bebel1982@users.noreply.github.com>
2023-05-21 17:13:16 +0200

    Update about.blade.php

--- resources/views/users/about.blade.php
+++ resources/views/users/about.blade.php
@@ -1,18 +1,62 @@
 <x-layoutIndex>
-    Welcome to Sherart, where creative minds from diverse artistic disciplines come together to collaborate and bring their visionary projects to life. We provide a dynamic and inclusive space for artists to connect, exchange ideas, and foster collaborations that transcend traditional boundaries.
 
-    At our core, we believe in the power of interdisciplinary collaboration. We understand that the most groundbreaking and awe-inspiring creations often emerge when artists from different areas join forces, bringing their unique perspectives and expertise to the table. Our platform serves as a catalyst for these transformative partnerships, enabling artists to explore uncharted territories and push the boundaries of their art.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Include the MDB Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdb@5.2.1/dist/css/mdb.min.css" integrity="sha384-pFHoPRyn9ur0c89LqgsrCCk0IE0Ck0HBhNc1NoRZDL6vksb0tACPePe7j8sxM0aW" crossorigin="anonymous">
+  <title>Nicely Styled Text with MDB Bootstrap</title>
+  <style>
+    .lead {
+      text-align: justify;
+      text-justify: inter-word;
+    }
+  </style>
+</head>
+<body>
+ <div class="justified-content">
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h1 class="text-center mb-4">Spherart</h1>
+        <p class="lead">
     
-    Whether you are a painter, musician, writer, filmmaker, dancer, or any other type of artist, our platform offers you a vibrant and supportive community. Connect with fellow creators, share your work, and find inspiration in the rich tapestry of talents that abound here. It's a place where artistic boundaries blur, and innovation thrives.
-    
-    Collaboration lies at the heart of our platform. Artists can easily connect with one another, form teams, and embark on ambitious joint projects. Looking for a composer to enhance your film score? Seeking a choreographer to bring movement to your theatrical production? Or perhaps you're a novelist searching for a cover artist for your next book. Whatever your creative aspirations, our platform provides the tools and connections to make your visions a reality.
-    
-    We value diversity and inclusivity, recognizing that different perspectives and backgrounds contribute to a richer creative ecosystem. Our platform celebrates artists from all walks of life, ensuring that everyone has a voice and an opportunity to shine. We actively foster a nurturing and collaborative environment where respect, openness, and mutual support thrive.
-    
-    Join our vibrant artistic community today and unlock the immense potential that lies within collaborative creation. Discover kindred spirits, forge lifelong connections, and embark on extraordinary artistic journeys. Together, we can break new ground, inspire one another, and shape the future of art.
-    
-    Welcome to our platform. Welcome to a world of limitless creative possibilities.
+        Welcome to Spherart where creative minds from diverse artistic disciplines come together to collaborate and bring their visionary projects to life. We provide a dynamic and inclusive space for artists to connect, exchange ideas, and foster collaborations that transcend traditional boundaries.</p>
+
+
+        <p class="lead">
+        At our core, we believe in the power of interdisciplinary collaboration. We understand that the most groundbreaking and awe-inspiring creations often emerge when artists from different areas join forces, bringing their unique perspectives and expertise to the table.</p>
+
+        <p class="lead">
+        Our platform serves as a catalyst for these transformative partnerships, enabling artists to explore uncharted territories and push the boundaries of their art. Whether you are a painter, musician, writer, filmmaker, dancer, or any other type of artist, our platform offers you a vibrant and supportive community. Connect with fellow creators, share your work, and find inspiration in the rich tapestry of talents that abound here. It's a place where artistic boundaries blur, and innovation thrives.</p>
+        
+        <p class="lead">
+        Collaboration lies at the heart of our platform. Artists can easily connect with one another, form teams, and embark on ambitious joint projects.Looking for a composer to enhance your film score? Seeking a choreographer to bring movement to your theatrical production? Or perhaps you're a novelist searching for a cover artist for your next book. Whatever your creative aspirations, our platform provides the tools and connections to make your visions a reality.</p>
+
+        <p class="lead">
+        We value diversity and inclusivity, recognizing that different perspectives and backgrounds contribute to a richer creative ecosystem. Our platform celebrates artists from all walks of life, ensuring that everyone has a voice and an opportunity to shine.</p>
+        
+        <p class="lead">
+        We actively foster a nurturing and collaborative environment where respect, openness, and mutual support thrive. Join our vibrant artistic community today and unlock the immense potential that lies within collaborative creation. Discover kindred spirits, forge lifelong connections, and embark on extraordinary artistic journeys. Together, we can break new ground, inspire one another, and shape the future of art. Welcome to our platform.</p>
+
+        <center><p class="lead">
+         Welcome to a world of limitless creative possibilities. 
+        </p></center>
+
+        
+            </div>
+        </div>
+    </div>
+  </div>
+  <!-- Include the MDB Bootstrap JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/mdb@5.2.1/dist/js/mdb.min.js" integrity="sha384-i60OmT4N9gZIlp0WyF7gO2BS0Yl/P0oULhU7uvC2o8XqoXf8oZZyfEz4ktv+qPDp" crossorigin="anonymous"></script>
+</body>
+</html>
+
     
 
+    
 
 </x-layoutIndex>
