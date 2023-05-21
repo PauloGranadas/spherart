87fc77a424be0a4771b8e4048e612c551f02993f
jaeri <jaeriadias@gmail.com>
2023-05-21 14:57:16 +0200

    .

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,17 +6,17 @@
         <!--Images to showcase the project-->
         <img src="{{$project->cover ? asset('storage/' . $project->cover) : asset('images/no-image.png')}}" class="img-fluid rounded mx-auto d-block mt-4" alt="Cover Image" id="project_showcase" style="object-fit: cover; max-height: 500px; width: 100%;"/>
       
-        <div class="position-absolute top-0 end-0 bg-dark text-white p-3 h-100" style="max-width: 30%; min-width: 20%; border-top-right-radius: 5px; border-bottom-right-radius: 5px; opacity: 0.8;">
+        <div class="position-absolute top-0 end-0 bg-primary text-white p-3 h-100" style="max-width: 30%; min-width: 20%; border-top-right-radius: 5px; border-bottom-right-radius: 5px; opacity: 0.9;">
           <!-- Project Title -->
           <h2>{{$project->name}}</h1>
           
           <!-- Author -->
-          <div class="mt-4">
-            <span>Author:</span>
+          <div class="mt-4" >
+            <span style="opacity: 1;">Author:</span>
             <div class="media">
-              <img src="path/to/author-photo.jpg" class="d-flex align-self-center mr-3 rounded-circle" alt="Author Photo" style="width: 50px;">
+              <img src="{{$project->user->avatar ? asset('storage/' . $project->user->avatar) : asset('images/no-image.png')}}" class="d-flex align-self-center mr-3 rounded-circle" alt="Author Photo" style="width: 50px; height: 50px; opacity: 1;">
               <div class="media-body">
-                <h6 class="mt-0">Author Name</h6>
+                <h6 class="mt-0">{{$project->user->nikname}}</h6>
               </div>
             </div>
           </div>
@@ -79,17 +79,7 @@
           <span class="badge badge-primary">Category 1</span>
           <span class="badge badge-primary">Category 2</span>
         </div>
-        
-        <!-- Author -->
-        <div class="mt-4">
-          <h5>Author:</h5>
-          <div class="media">
-            <img src="path/to/author-photo.jpg" class="d-flex align-self-center mr-3 rounded-circle" alt="Author Photo" style="width: 50px;">
-            <div class="media-body">
-              <h6 class="mt-0">Author Name</h6>
-            </div>
-          </div>
-        </div>
+       
   </div>
 
 
