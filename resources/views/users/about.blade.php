ed7e763d8618528267f7a1df74d9e47434d97660
jaeri <jaeriadias@gmail.com>
2023-05-23 06:38:22 +0200

    update about

--- resources/views/users/about.blade.php
+++ resources/views/users/about.blade.php
@@ -1,17 +1,82 @@
 <x-layoutIndex>
-    Welcome to Sherart, where creative minds from diverse artistic disciplines come together to collaborate and bring their visionary projects to life. We provide a dynamic and inclusive space for artists to connect, exchange ideas, and foster collaborations that transcend traditional boundaries.
 
-    At our core, we believe in the power of interdisciplinary collaboration. We understand that the most groundbreaking and awe-inspiring creations often emerge when artists from different areas join forces, bringing their unique perspectives and expertise to the table. Our platform serves as a catalyst for these transformative partnerships, enabling artists to explore uncharted territories and push the boundaries of their art.
+<style>
+
     
-    Whether you are a painter, musician, writer, filmmaker, dancer, or any other type of artist, our platform offers you a vibrant and supportive community. Connect with fellow creators, share your work, and find inspiration in the rich tapestry of talents that abound here. It's a place where artistic boundaries blur, and innovation thrives.
+
+
+    * {
+      box-sizing: border-box;
+    }
+
+    .lead {
+      text-align: justify;
+      text-justify: inter-word;
+    }
+
+    .col-md-8 {
+      float: left;
+      width: 50%;
+      padding: 50px;
+      height: 300px;
+    }
+
+    .h1{
+        margin-top:50px;
+       
+        text-align:bottom;
+        
+    }
+
+        .title{
+            font-style:italic;
+            text-align:center;
+            
+        }
     
-    Collaboration lies at the heart of our platform. Artists can easily connect with one another, form teams, and embark on ambitious joint projects. Looking for a composer to enhance your film score? Seeking a choreographer to bring movement to your theatrical production? Or perhaps you're a novelist searching for a cover artist for your next book. Whatever your creative aspirations, our platform provides the tools and connections to make your visions a reality.
-    
-    We value diversity and inclusivity, recognizing that different perspectives and backgrounds contribute to a richer creative ecosystem. Our platform celebrates artists from all walks of life, ensuring that everyone has a voice and an opportunity to shine. We actively foster a nurturing and collaborative environment where respect, openness, and mutual support thrive.
-    
-    Join our vibrant artistic community today and unlock the immense potential that lies within collaborative creation. Discover kindred spirits, forge lifelong connections, and embark on extraordinary artistic journeys. Together, we can break new ground, inspire one another, and shape the future of art.
-    
-    Welcome to our platform. Welcome to a world of limitless creative possibilities.
+  </style>
+
+
+<h1 class="text-center my-4" style="font-size: 50px;">Spherart</h1>
+<div class="title" style="font-size: 50px;">
+
+
+        <p class="lead" > 
+        <center>"Welcome to a world of limitless creative possibilities"</center>
+          </p>
+
+</div>
+  <div class="justified-content">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-8">
+        <b><strong></strong></b>
+          <p class="lead">
+            Welcome to Spherart where creative minds from diverse artistic disciplines come together to collaborate and bring their visionary projects to life. We provide a <b><strong>dynamic and inclusive</strong></b> space for artists to connect, exchange ideas, and foster collaborations that transcend traditional boundaries.
+          </p>
+          <p class="lead">
+            At our core, we believe in the power of <b><strong> interdisciplinary collaboration</strong></b>. We understand that the most groundbreaking and awe-inspiring creations often emerge when artists from different areas join forces, bringing their unique perspectives and expertise to the table.
+          </p>
+          <p class="lead">
+            Our platform serves as a catalyst for these transformative partnerships, enabling artists to explore uncharted territories and push the boundaries of their art. Whether you are a <b> <strong>painter, musician, writer, filmmaker, dancer, </strong></b> or any other type of artist, our platform offers you a vibrant and supportive community.<b><strong> Connect</strong></b> with fellow creators, share your work, and find inspiration in the rich tapestry of talents that abound here. It's a place where artistic boundaries blur, and innovation thrives.
+          </p>
+        </div>
+
+        <div class="col-md-8">
+          <p class="lead">
+            Collaboration lies at the heart of our platform. Artists can easily connect with one another, form teams, and embark on<b> <strong>ambitious joint projects</strong></b>. Looking for a composer to enhance your film score? Seeking a choreographer to bring movement to your theatrical production? Or perhaps you're a novelist searching for a cover artist for your next book. Whatever your creative aspirations, <b><strong>our platform provides the tools</strong></b> and connections to make your visions a reality.
+          </p>
+          <p class="lead">
+            We value <b><strong>diversity and inclusivity</strong></b>, recognizing that different perspectives and backgrounds contribute to a richer creative ecosystem. Our platform celebrates artists from all walks of life, ensuring that everyone has a voice and an opportunity to shine.
+          </p>
+          <p class="lead">
+            We actively foster a nurturing and collaborative environment where respect, openness, and mutual support thrive. Join our vibrant artistic community today and unlock the immense potential that lies within collaborative creation. Discover kindred spirits, forge lifelong connections, and embark on extraordinary artistic journeys. Together, we can break new ground, inspire one another, and shape <b><strong>the future of art</strong></b>.
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </div>
     
 
 
